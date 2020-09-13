ba47a9510c3d044f416608b22707a912f5851d60
Ali <a.a.ghanati@gmail.com>
2020-09-13 11:43:48 +0430

    finished!

--- mine_sweeper.js
+++ mine_sweeper.js
@@ -1,10 +1,24 @@
 'use strict';
 
+class Cell {
+    constructor() {
+        this.inside = '';
+        this.flagged = false;
+        this.openned = false;
+        this.exploded = false;
+    }
+}
+
 function newGame() {
+    end = false;
     const body = document.getElementsByTagName('body')[0];
     document.getElementById('mine field').remove();
     document.getElementById('new game').remove();
     document.getElementById('hr').remove();
+    try {
+        document.getElementsByTagName('hr')[0].remove();
+        document.getElementsByTagName('h1')[0].remove();
+    } catch (e) {}
     const easy = document.createElement('input');
     easy.type = 'submit';
     easy.value = 'Easy';
@@ -66,16 +80,12 @@ function difficulty(diff) {
 }
 
 function init() {
-    const flagged = [];
-    const field = [];
+    field = [];
     const canvas = document.getElementById('mine field');
-    const ctx = canvas.getContext('2d');
     for (let i = 0; i < canvas.height / 24; i++) {
-        flagged.push([]);
         field.push([]);
         for (let j = 0; j < canvas.width / 24; j++) {
-            flagged[i].push('');
-            field[i].push('');
+            field[i].push(new Cell());
         }
     }
     const mines = canvas.width === 240 ? 10 :
@@ -88,35 +98,264 @@ function init() {
             i = Math.floor(Math.random() * (canvas.height / 24));
             j = Math.floor(Math.random() * (canvas.width / 24));
         }
-        field[i][j] = 'mine';
+        field[i][j].inside = 'mine';
     }
-    draw();
-    canvas.addEventListener('mousemove', hover);
+    field = calc_nums(field);
+    draw(field);
+    canvas.addEventListener('mousemove', hover(field));
+    canvas.addEventListener('click', click(field));
+    canvas.addEventListener('contextmenu', rightClick(field));
 }
 
-function draw() {
+function click(field) {
+    return (clickEvent) => {
+        if (!end) {
+            if (!(field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].flagged ||
+                field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].openned)) {
+                if (field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].inside !== 'mine') {
+                    field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].openned = true;
+                    if (field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].inside === '0') {
+                        revealAround(field, Math.floor(clickEvent.offsetY / 24), Math.floor(clickEvent.offsetX / 24));
+                    }
+                    end = checkEnd(field);
+                    if (end) {
+                        document.body.appendChild(document.createElement('hr'));
+                        const win = document.createElement('h1');
+                        win.style.color = 'green';
+                        win.innerText = 'You Win!';
+                        document.body.appendChild(win);
+                    }
+                }
+                else {
+                    field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].exploded = true;
+                    end = true;
+                    draw_mines(field);
+                }
+                draw(field);
+            }
+        }
+    }
+}
+
+function draw_mines(field) {
     const canvas = document.getElementById('mine field');
     const ctx = canvas.getContext('2d');
-    const cell = new Image();
-    cell.src = 'assets/Cell.png';
-    cell.onload = () => {
-        for (let i = 0; i < canvas.width; i += 24) {
-            for (let j = 0; j < canvas.height; j += 24) {
-                ctx.drawImage(cell, i, j);
+    const mine = new Image();
+    mine.src = 'assets/RevealedMineCell.png';
+    mine.onload = () => {
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                if (field[i][j].inside === 'mine' && !field[i][j].exploded) {
+                    ctx.drawImage(mine, j * 24, i * 24);
+                }
             }
         }
     };
 }
 
-function hover(event) {
-    const ctx = document.getElementById('mine field').getContext('2d');
-    draw();
-    const cellOver = new Image();
-    cellOver.src = 'assets/CellOver.png';
-    cellOver.onload = () => {
-        ctx.drawImage(cellOver, Math.floor(event.offsetX / 24) * 24, Math.floor(event.offsetY / 24) * 24)
+function rightClick(field) {
+    return (clickEvent) => {
+        clickEvent.preventDefault();
+        if (!end) {
+            if (!(field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].openned)) {
+                field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].flagged =
+                    !field[Math.floor(clickEvent.offsetY / 24)][Math.floor(clickEvent.offsetX / 24)].flagged;
+                draw(field);
+            }
+        }
+    }
+}
+
+function draw(field) {
+    const canvas = document.getElementById('mine field');
+    const ctx = canvas.getContext('2d');
+    const cell = new Image();
+    cell.src = 'assets/Cell.png';
+    cell.onload = () => {
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                if (!(field[i][j].openned || field[i][j].flagged)) {
+                    ctx.drawImage(cell, j * 24, i * 24);
+                }
+            }
+        }
+    };
+    const flagged = new Image();
+    flagged.src = 'assets/FlaggedCell.png';
+    flagged.onload = () => {
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                if (field[i][j].flagged) {
+                    ctx.drawImage(flagged, j * 24, i * 24);
+                }
+            }
+        }
+    };
+    const open = new Image();
+    open.src = 'assets/EmptyCell.png';
+    open.onload = () => {
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                if (field[i][j].openned) {
+                    ctx.drawImage(open, j * 24, i * 24);
+                    if (field[i][j].inside !== 'mine' && field[i][j].inside !== '0') {
+                        switch (field[i][j].inside) {
+                            case '1':
+                                ctx.fillStyle = 'blue';
+                                break;
+                            case '2':
+                                ctx.fillStyle = 'green';
+                                break;
+                            case '3':
+                                ctx.fillStyle = 'red';
+                                break;
+                            case '4':
+                                ctx.fillStyle = 'purple';
+                                break;
+                            case '5':
+                                ctx.fillStyle = 'maroon';
+                                break;
+                            case '6':
+                                ctx.fillStyle = 'turquoise';
+                                break;
+                            case '7':
+                                ctx.fillStyle = 'black';
+                                break;
+                            case '8':
+                                ctx.fillStyle = 'gray';
+                                break;
+                        }
+                        ctx.font = '24px Georgia';
+                        ctx.fillText(field[i][j].inside, j * 24 + 6, i * 24 + 18);
+                    }
+                }
+            }
+        }
+    };
+    const exploded = new Image();
+    exploded.src = 'assets/ExplodedMineCell.png';
+    exploded.onload = () => {
+        let flag = false;
+        for (let i = 0; i < field.length; i++) {
+            for (let j = 0; j < field[i].length; j++) {
+                if (field[i][j].exploded) {
+                    ctx.drawImage(exploded, j * 24, i * 24);
+                    break;
+                }
+            }
+            if (flag) break;
+        }
     };
 }
 
+function hover(field) {
+    return (event) => {
+        if (!end) {
+            const ctx = document.getElementById('mine field').getContext('2d');
+            draw(field);
+            const cellOver = new Image();
+            cellOver.src = 'assets/CellOver.png';
+            cellOver.onload = () => {
+                if (!(field[Math.floor(event.offsetY / 24)][Math.floor(event.offsetX / 24)].flagged ||
+                    field[Math.floor(event.offsetY / 24)][Math.floor(event.offsetX / 24)].openned)) {
+                    ctx.drawImage(cellOver, Math.floor(event.offsetX / 24) * 24, Math.floor(event.offsetY / 24) * 24)
+                }
+            };
+        }
+    }
+}
+
+function calc_nums(field) {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j].inside === 'mine') continue;
+
+            let counter = 0;
+            try {
+                if (field[i][j - 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i - 1][j - 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i + 1][j - 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i - 1][j].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i + 1][j].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i - 1][j + 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i][j + 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            try {
+                if (field[i + 1][j + 1].inside === 'mine') {
+                    counter++;
+                }
+            } catch (e) {}
+            field[i][j].inside = counter.toString();
+        }
+    }
+    return field;
+}
+
+function checkEnd(field) {
+    for (let i = 0; i < field.length; i++) {
+        for (let j = 0; j < field[i].length; j++) {
+            if (field[i][j].inside !== 'mine' && !field[i][j].openned) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function revealAround(field, i, j) {
+    try {
+        field[i][j - 1].openned = true;
+    } catch (e) {}
+    try {
+        field[i - 1][j - 1].openned = true;
+    } catch (e) {}
+    try {
+        field[i + 1][j - 1].openned = true;
+    } catch (e) {}
+    try {
+        field[i - 1][j].openned = true;
+    } catch (e) {}
+    try {
+        field[i + 1][j].openned = true;
+    } catch (e) {}
+    try {
+        field[i - 1][j + 1].openned = true;
+    } catch (e) {}
+    try {
+        field[i][j + 1].openned = true;
+    } catch (e) {}
+    try {
+        field[i + 1][j + 1].openned = true;
+    } catch (e) {}
+}
+
+let field = [];
+let end = false;
 document.getElementById('new game').addEventListener('click', newGame);
 init();
